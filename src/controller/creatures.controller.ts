74d11634ff8c7889a056aefa7d770c1bc47a2778
Enzo <aypierre30@gmail.com>
2025-01-24 10:02:59 +0100

    feat: prisma implementation

--- src/controller/creatures.controller.ts
+++ src/controller/creatures.controller.ts
@@ -1,31 +1,11 @@
+import { getAllCreatures } from "@/services/creatures.service.js";
 import { Request, Response } from "express";
-import * as CreatureService from "@/services/creatures.service";
 
 export const getCreatures = async (req: Request, res: Response) => {
     try {
-        const creatures = await CreatureService.getAllCreatures();
+        const creatures = await getAllCreatures();
         res.json(creatures);
     } catch {
-        res.status(500).json({ error: "Failed to fetch creatures" });
-    }
-};
-
-export const assignCreature = async (req: Request, res: Response) => {
-    try {
-        const { matchId, creatureId } = req.body;
-        const result = await CreatureService.assignCreature(matchId, creatureId);
-        res.json(result);
-    } catch {
-        res.status(400).json({ error: "An error occurred while assign a creature" });
-    }
-};
-
-export const deleteCreature = async (req: Request, res: Response) => {
-    try {
-        const { creatureId } = req.params;
-        const result = await CreatureService.deleteCreature(Number(creatureId));
-        res.json(result);
-    } catch {
-        res.status(404).json({ error: "An error occurred while deleting the creature" });
+        res.status(500).json({ error: "Impossible de récupérer les créatures" });
     }
 };
--- src/services/creatures.service.ts
+++ src/services/creatures.service.ts
@@ -1,29 +1,7 @@
-import { Creature, creatures } from "@/model/creatures.model";
+import { prisma } from "./index.js";
 
-export const getAllCreatures = async (): Promise<Creature[]> => {
-    return creatures; // TODO: Implement a database
-};
 
-export const assignCreature = async (matchId: number, creatureId: number) => {
-    const creature = creatures.find((c) => c.id === creatureId);
-    if (!creature) {
-        throw new Error("Creature not found");
-    }
-    return {
-        message: "Creature assigned successfully",
-        matchId,
-        creature,
-    };
-};
-
-export const deleteCreature = async (creatureId: number) => {
-    const index = creatures.findIndex((c) => c.id === creatureId);
-    if (index === -1) {
-        throw new Error("Creature not found");
-    }
-    const [deletedCreature] = creatures.splice(index, 1);
-    return {
-        message: "Creature sold successfully",
-        creditsRefunded: deletedCreature.price,
-    };
+export const getAllCreatures = async () => {
+    const creatureList = await prisma.creature.findMany();
+    return creatureList
 };
