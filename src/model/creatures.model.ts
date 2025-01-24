2d5182835760abd4a93a8feea7ba994b5fcdb7cb
Enzo <aypierre30@gmail.com>
2025-01-24 10:07:12 +0100

    fix: routes change

--- src/model/creatures.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Creature {
-    id: number;
-    name: string;
-    type: string;
-    attackPoint: number;
-    defensePoint: number;
-    rarity: number;
-}
--- src/services/creatures.service.ts
+++ src/services/creatures.service.ts
@@ -1,6 +1,5 @@
 import { prisma } from "./index.js";
 
-
 export const getAllCreatures = async () => {
     const creatureList = await prisma.creature.findMany();
     return creatureList
