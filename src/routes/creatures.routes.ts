521a2f762d7bfe76b7822921f874a273fc68a92a
Enzo <aypierre30@gmail.com>
2025-01-24 10:07:12 +0100

    fix: routes change

--- src/routes/creatures.routes.ts
+++ src/routes/creatures.routes.ts
@@ -4,7 +4,5 @@ import * as CreatureController from "@/controller/creatures.controller";
 const router = express.Router();
 
 router.get("/", CreatureController.getCreatures);
-router.post("/assign", CreatureController.assignCreature);
-router.delete("/:creatureId", CreatureController.deleteCreature);
 
 export default router;
